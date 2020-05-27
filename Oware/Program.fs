4b7c941ca177c1a34b2294599f728fa6ee04d8ee
frnkhakim <61003246+frnkhakim@users.noreply.github.com>
2020-05-27 17:13:44 +0200

    I added to the "start position"

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -52,11 +52,35 @@ let start position ={
 }
 
 
+
+
 let getSeeds n board = failwith "Not implemented"
 //i am worked only with frank
 let useHouse n board = failwith "Not implemented"
 
-let start position = failwith "Not implemented"
+//initial position of all players
+let start position ={
+
+    a = 4;
+    b = 4;
+    c = 4;
+    d = 4;
+    e = 4;
+    f = 4;
+
+    g = 4;
+    h = 4;
+    i = 4;
+    j = 4;
+    k = 4;
+    l = 4;
+
+    north_points = 4;
+    south_points = 4;
+
+    turn = position;
+
+}
 
 let score board = failwith "Not implemented"
 
