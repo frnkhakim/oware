d828b5e603fa4f8da6bec54d8b75e538a0310a70
frnkhakim <61003246+frnkhakim@users.noreply.github.com>
2020-05-27 17:12:26 +0200

    I add the "type board"

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -4,6 +4,54 @@ type StartingPosition =
     | South
     | North
 
+type board = {
+
+    a:int;
+    b:int;
+    c:int;
+    d:int;
+    e:int;
+    f:int;
+
+    g:int;
+    h:int;
+    i:int;
+    j:int;
+    k:int;
+    l:int;
+
+    north_points:int;
+    south_points:int;
+
+    turn: StartingPosition;
+
+    }
+
+//initial position of all players
+let start position ={
+
+    a = 4;
+    b = 4;
+    c = 4;
+    d = 4;
+    e = 4;
+    f = 4;
+
+    g = 4;
+    h = 4;
+    i = 4;
+    j = 4;
+    k = 4;
+    l = 4;
+
+    north_points = 4;
+    south_points = 4;
+
+    turn = position;
+
+}
+
+
 let getSeeds n board = failwith "Not implemented"
 //i am worked only with frank
 let useHouse n board = failwith "Not implemented"
